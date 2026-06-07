22e4c7a6079693f3483569216a8db48d9056235a
Kanika Rathore <kanikalrathore@gmail.com>
2026-06-07 22:38:29 +0530

    Add BERT tokenizer for text encoding

--- Tokenization /BERT_tokenizer.py
+++ Tokenization /BERT_tokenizer.py
@@ -1,1 +1,7 @@
 
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
+encoded = tokenizer('Hello, I am learning about LLMs!', return_tensors='pt')
+print('Input IDs:', encoded['input_ids'])
+print('Tokens:', tokenizer.convert_ids_to_tokens(encoded['input_ids'][0]))
+
